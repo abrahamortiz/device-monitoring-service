6ca083277884b47fb7dcaee4e399b89e0f2a2773
Abraham Ortiz <abrahamortizu@gmail.com>
2026-02-25 15:32:11 +0100

    feat: add app class

---
+++ apps/monitoring-service/src/infrastructure/http/app.ts
@@ -0,0 +1,51 @@
+import type { FastifyInstance } from "fastify";
+import type { NodeEnv } from "../../config/app.config.ts";
+import type { Container } from "../di/container.ts";
+import { fastify } from "fastify";
+
+export class App {
+  private container: Container;
+  private fastifyInstance: FastifyInstance;
+
+  constructor(container: Container, nodeEnv: NodeEnv) {
+    this.container = container;
+
+    this.fastifyInstance = fastify({
+      logger:
+        nodeEnv === "production"
+          ? { level: "error" }
+          : {
+              level: "info",
+              transport: {
+                target: "pino-pretty",
+                options: {
+                  colorize: true,
+                  translateTime: "SYS:standard",
+                  ignore: "pid,hostname",
+                },
+              },
+            },
+    });
+  }
+
+  public async registerRoutes() {
+    this.fastifyInstance.get("/health", async () => ({ ok: true }));
+    this.container.getDeviceRoutes().register(this.fastifyInstance);
+  }
+
+  public async init() {
+    await this.registerRoutes();
+  }
+
+  public async close() {
+    await this.fastifyInstance.close();
+  }
+
+  public getInstance(): FastifyInstance {
+    return this.fastifyInstance;
+  }
+
+  public getContainer(): Container {
+    return this.container;
+  }
+}
